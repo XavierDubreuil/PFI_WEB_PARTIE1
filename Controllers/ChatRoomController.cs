db9cae244ec05f7255f28907fcddd7df9d59dbd6
XavierDubreuil <116843174+XavierDubreuil@users.noreply.github.com>
2024-04-15 10:28:53 -0400

    Add files via upload

---
+++ Controllers/ChatRoomController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ChatManager.Controllers
+{
+    public class ChatRoomController : Controller
+    {
+        // GET: ChatRoom
+        public ActionResult Index()
+        {
+            Session["LastAction"] = "/ChatRoom/Index";
+            return View();
+        }
+    }
+}
---
+++ Controllers/FriendShipsController.cs
@@ -0,0 +1,19 @@
+﻿using ChatManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MoviesDBManager.Controllers
+{
+    [OnlineUsers.UserAccess]
+    public class FriendShipsController : Controller
+    {
+        public ActionResult Index()
+        {
+            Session["LastAction"] = "FriendShips/index";
+            return View();
+        }
+    }
+}
