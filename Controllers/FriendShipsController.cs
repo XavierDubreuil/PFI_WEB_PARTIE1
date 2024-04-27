09f1a46f8045b2cdb105875b7bc1154bb5545ba0
GermainDavid <david.germain0410@gmail.com>
2024-04-27 18:47:42 -0400

    Partie_David_Fini

--- Controllers/FriendShipsController.cs
+++ Controllers/FriendShipsController.cs
@@ -34,7 +34,33 @@ namespace MoviesDBManager.Controllers
         {
             var relations = DB.Relations.FindRelation(Idusr1, Idusr2);
             relations.rel1.Rapport = TypeRapport.Demande_Envoyée;
+            DB.Relations.Update(relations.rel1);
             relations.rel2.Rapport = TypeRapport.Requête_Entrante;
+            DB.Relations.Update(relations.rel2);
+        }
+        public void AccepterDemande(int Idusr1, int Idusr2)
+        {
+            var relations = DB.Relations.FindRelation(Idusr1, Idusr2);
+            relations.rel1.Rapport = TypeRapport.Amis;
+            DB.Relations.Update(relations.rel1);
+            relations.rel2.Rapport = TypeRapport.Amis;
+            DB.Relations.Update(relations.rel2);
+        }
+        public void RefuserDemande(int Idusr1, int Idusr2)
+        {
+            var relations = DB.Relations.FindRelation(Idusr1, Idusr2);
+            relations.rel1.Rapport = TypeRapport.A_Refusé_Demande;
+            DB.Relations.Update(relations.rel1);
+            relations.rel2.Rapport = TypeRapport.Demande_Refusé;
+            DB.Relations.Update(relations.rel2);
+        }
+        public void RetirerDemande(int Idusr1, int Idusr2)
+        {
+            var relations = DB.Relations.FindRelation(Idusr1, Idusr2);
+            relations.rel1.Rapport = TypeRapport.Pas_Encore_Ami;
+            DB.Relations.Update(relations.rel1);
+            relations.rel2.Rapport = TypeRapport.Pas_Encore_Ami;
+            DB.Relations.Update(relations.rel2);
         }
     }
 }
