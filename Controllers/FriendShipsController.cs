5e0a9fdb79e8fe349adb7d9d93d5a6d38b90e098
XavierDubreuil <202245543@edu.clg.qc.ca>
2024-04-29 12:06:57 -0400

    Filtre (pas fini)

--- Controllers/FriendShipsController.cs
+++ Controllers/FriendShipsController.cs
@@ -1,6 +1,7 @@
 ﻿using ChatManager.Models;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -19,14 +20,26 @@ namespace MoviesDBManager.Controllers
         {
             if (forceRefresh || DB.Relations.HasChanged || OnlineUsers.HasChanged() || DB.Users.HasChanged)
             {
-                var listesParUser = DB.Relations.ToList().GroupBy(c => c.UsersId.usr1).ToList();
-                foreach (var liste in listesParUser) 
-                { 
-                    if(liste.ElementAt(0).UsersId.usr1 == OnlineUsers.GetSessionUser().Id)
+                if (Session["ListeFlitrer"] == null)
+                {
+                    var listesParUser = DB.Relations.ToList().GroupBy(c => c.UsersId.usr1).ToList();
+                    foreach (var liste in listesParUser)
+                    {
+                        if (liste.ElementAt(0).UsersId.usr1 == OnlineUsers.GetSessionUser().Id)
+                        {
+                            return PartialView(liste.OrderBy(c => DB.Users.Get(c.UsersId.usr2).FirstName));
+                        }
+                    }
+                }
+                else
+                {
+                    var test6 = (List<Relation>)Session["ListeFlitrer"];
+                    foreach(var liste in test6) 
                     {
                         return PartialView(liste.OrderBy(c => DB.Users.Get(c.UsersId.usr2).FirstName));
                     }
                 }
+
             }
             return null;
         }
@@ -61,6 +74,69 @@ namespace MoviesDBManager.Controllers
             DB.Relations.Update(relations.rel1);
             relations.rel2.Rapport = TypeRapport.Pas_Encore_Ami;
             DB.Relations.Update(relations.rel2);
+        }
+        public void Filtre(string[] test)
+        {
+            var test2 = test[0].Split(',');
+            bool block = false;
+            List<TypeRapport> listRap = new List<TypeRapport>();
+            foreach(var test3 in test2)
+            {
+                switch (test3)
+                {
+                    case "Pas_Encore_Ami":
+                        listRap.Add(TypeRapport.Pas_Encore_Ami);
+                        break;
+                    case "Requête_Entrante":
+                        listRap.Add(TypeRapport.Requête_Entrante);
+                        break;
+                    case "Demande_Envoyée":
+                        listRap.Add(TypeRapport.Demande_Envoyée);
+                        break;
+                    case "Amis":
+                        listRap.Add(TypeRapport.Amis);
+                        break;
+                    case "DemandeRefuser":
+                        listRap.Add(TypeRapport.Demande_Refusé);
+                        listRap.Add(TypeRapport.A_Refusé_Demande);
+                        break;
+                    case "Bloquer":
+                        block = true;
+                        break;
+                }
+            }
+            var listesParUser = DB.Relations.ToList().GroupBy(c => c.UsersId.usr1).ToList();
+            List<Relation> TestList = new List<Relation>();
+            foreach (var liste in listesParUser)
+            {
+                if (liste.ElementAt(0).UsersId.usr1 == OnlineUsers.GetSessionUser().Id)
+                {
+                    foreach (var elem in liste)
+                    {
+                        foreach(var item in listRap)
+                        {
+                            if(!block)
+                            {
+                                var user = DB.Users.Get(elem.UsersId.usr2);
+
+                                if (elem.Rapport == item && !user.Blocked)
+                                {
+                                    TestList.Add(elem);
+                                }
+                            }
+                            else
+                            {
+                                if (elem.Rapport == item)
+                                {
+                                    TestList.Add(elem);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(TestList);
+            Session["ListeFlitrer"] = TestList;
         }
     }
 }
