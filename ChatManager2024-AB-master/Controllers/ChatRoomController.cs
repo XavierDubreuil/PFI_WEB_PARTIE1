4517d86154eb12d79c95f1458e4feb690520b948
GermainDavid <116567996+GermainDavid@users.noreply.github.com>
2024-05-15 17:21:34 -0400

    Revert "Réglages, changement de format et correction de bugs"
    This reverts commit ae74e7315c3bfb2165e533539cf1a5033d4383d6.

--- ChatManager2024-AB-master/Controllers/ChatRoomController.cs
+++ ChatManager2024-AB-master/Controllers/ChatRoomController.cs
@@ -17,52 +17,10 @@ namespace ChatManager.Controllers
             Session["SelecId"] = 0;
             return View();
         }
-        public ActionResult Conv(bool forceRefresh = false)
-        {
-            if (forceRefresh || OnlineUsers.HasChanged() || DB.Friendships.HasChanged || DB.Messages.HasChanged )
-            {
-                var loggedUser = OnlineUsers.GetSessionUser();
-                //user Sélectionné
-                User userSelec;
-                if ((int)Session["SelecId"] == 0)
-                    userSelec = null;
-                else
-                    userSelec = DB.Users.Get((int)Session["SelecId"]);
-                //liste messages
-                IEnumerable<IEnumerable<Message>> listMessages;
-                if (userSelec == null)
-                    listMessages = null;
-                else
-                    listMessages = DB.Messages.GetConv(OnlineUsers.GetSessionUser().Id, (int)Session["SelecId"]);
 
-                //liste d'amis
-                List<User> filteredUsers = new List<User>(); 
-                List<User> users =
-                    DB.Users.SortedUsers().ToList().Where(u => u.Verified).ToList();
-                foreach (User user in users)
-                {
-                    if (DB.Friendships.RelationStatus(loggedUser.Id, user.Id) == EnumRelationStatus.Friend)
-                    {
-                        filteredUsers.Add(user);
-                    }
-
-                }
-                //Vérif si une amitié a changé
-                if(userSelec != null && !DB.Friendships.AreFriends(OnlineUsers.GetSessionUser().Id, (int)Session["SelecId"]))
-                {
-                    userSelec = null;
-                    listMessages = null;
-                    Session["SelecId"] = 0;
-                }
-                (IEnumerable<User> amis, User user,IEnumerable<IEnumerable<Message>> messages) retour = (filteredUsers, userSelec, listMessages);
-                return PartialView(retour);
-            }
-
-            return null;
-        }
         public ActionResult Friends(bool forceRefresh = false)
         {
-            if (forceRefresh || DB.Friendships.HasChanged)
+            if (forceRefresh || DB.Friendships.HasChanged || OnlineUsers.HasChanged() || DB.Users.HasChanged)
             {
                 var loggedUser = OnlineUsers.GetSessionUser();
                 List<User> filteredUsers = new List<User>();;
@@ -83,10 +41,8 @@ namespace ChatManager.Controllers
         }
         public ActionResult Message(int usrId = 0, bool forceRefresh = false)
         {
-            if (forceRefresh || DB.Friendships.HasChanged || DB.Messages.HasChanged)
+            if (forceRefresh || DB.Friendships.HasChanged || OnlineUsers.HasChanged() || DB.Users.HasChanged)
             {
-
-
                 if (usrId == 0 && (int)Session["SelecID"] == 0)
                 {
                     return null;
@@ -95,11 +51,6 @@ namespace ChatManager.Controllers
                 {
                     Session["SelecID"] = usrId;
                 }
-                //if (!DB.Friendships.AreFriends(OnlineUsers.GetSessionUser().Id, (int)Session["SelecID"]))
-                //{
-                //    Session["SelecID"] = 0;
-                //    return PartialView();
-                //}
                 usrId = (int)Session["SelecID"];
                 var listMessages = DB.Messages.GetConv(OnlineUsers.GetSessionUser().Id, usrId);
                 var usr = DB.Users.Get(usrId);
@@ -115,11 +66,8 @@ namespace ChatManager.Controllers
                 var objMessage = new Message(OnlineUsers.GetSessionUser().Id, (int)Session["SelecID"]);
                 objMessage.content = message;
                 DB.Messages.Add(objMessage);
+                Message(0,true);
             }
-        }
-        public void SetSelec(int usrID)
-        {
-            Session["SelecId"] = usrID;
         }
     }
 }
